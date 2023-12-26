f9ae06511d52d4ea95af425f32976f5fc32328e4
Ashkan Zeinolabedini <ashkan-oakville@outlook.com>
2023-12-26 16:41:41 -0500

    normalize the kernel

--- gaussian_filter.py
+++ gaussian_filter.py
@@ -1,9 +1,20 @@
 import numpy as np
 import cv2
 
-def image_convolution(kernel: np.ndarray, image: np.ndarray):
+def image_convolution(kernel: np.ndarray, image: np.ndarray) -> np.ndarray:
+    i = 0
+    j = 0
+    img_size = image.shape # tuple of image dimension
 
-    pass
+    output = np.zeros(img_size) # output image array
+
+    while i < img_size[0]:
+        while j < img_size[1]:
+
+            j += 1
+        i += 1
+
+    return output
 
 def create_gaussian_kernel(size: int, sigma=1) -> np.ndarray:
     """
@@ -11,6 +22,7 @@ def create_gaussian_kernel(size: int, sigma=1) -> np.ndarray:
 
     :param size: The dimension of the size x size array. Should be an odd number
     :param sigma: The standard deviation
+    :param factor: the factor to make kernel values integer
     :return: A size x size gaussian filter kernel
     """
     fltr = np.zeros((size, size)) # initialize (size X size) gaussian filter
@@ -33,7 +45,12 @@ def create_gaussian_kernel(size: int, sigma=1) -> np.ndarray:
         j = 0
         i += 1
 
+    # normalize the filter
+    fltr /= np.sum(fltr)
+
     return fltr
 
 if __name__ == "__main__":
-    print(create_gaussian_kernel(5))
+    kern = create_gaussian_kernel(5)
+    print(kern)
+    print(kern.shape)
