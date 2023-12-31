a053f3117fcbca3e2f2c865e4d7a1338c31567b2
Ashkan Zeinolabedini <ashkan-oakville@outlook.com>
2023-12-30 19:27:25 -0500

    fixed imports for refactor of methods to utilities.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import  image_filter as imf
+import utilities as util
 import sys
 import os
 
@@ -17,10 +18,10 @@ def create_gradient(img: np.ndarray)-> np.ndarray:
 
     sobel_kernels = imf.get_sobel_kernels()
 
-    gaussian_filtered = imf.image_convolution(img, gaussian_kernel)
+    gaussian_filtered = util.image_convolution(img, gaussian_kernel)
 
-    sobel_x = imf.image_convolution(gaussian_filtered, sobel_kernels[0])
-    sobel_y = imf.image_convolution(gaussian_filtered, sobel_kernels[1])
+    sobel_x = util.image_convolution(gaussian_filtered, sobel_kernels[0])
+    sobel_y = util.image_convolution(gaussian_filtered, sobel_kernels[1])
 
     gradient = imf.combine_sobel(sobel_x, sobel_y)
 
@@ -32,16 +33,18 @@ if __name__ == '__main__':
         print("please enter a file path.")
     elif len(sys.argv) == 2:
         img = open_img_grey(sys.argv[1])
-        gradient = create_gradient(img)
+        grad = create_gradient(img)
 
         filename = sys.argv[1].split("/")
         filename = filename[-1]
 
-        cv2.imwrite(f"output_{filename}", gradient)
+        cv2.imwrite(f"output_{filename}", grad)
+
     elif len(sys.argv) == 3:
         img = open_img_grey(sys.argv[1])
-        gradient = create_gradient(img)
+        grad = create_gradient(img)
 
-        cv2.imwrite(f"{sys.argv[2]}.jpg", gradient)
+        cv2.imwrite(f"{sys.argv[2]}.jpg", grad)
+        
     else:
         print("too many arguments")
